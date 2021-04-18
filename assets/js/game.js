1565c476f7b70037b4ba84e46438a921de04224e
Kimberly Salas <salask0104@gmail.com>
2021-04-17 21:52:39 -0700

    added const in js

---
+++ assets/js/game.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorALL('.choice-text'));
+const question = document.querySelector('#question');
+const question = document.querySelector('#question');
+const question = document.querySelector('#question');
