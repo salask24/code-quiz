980bc9497c8dfd7a1cd52e4dd3353f588d3407da
Kimberly Salas <salask0104@gmail.com>
2021-04-18 08:51:45 -0700

    added questions and answers to template

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,51 @@
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorALL('.choice-text'));
-const question = document.querySelector('#question');
-const question = document.querySelector('#question');
-const question = document.querySelector('#question');
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question:
+            "",
+        choice1: "f",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: 4,
+    },
+    {
+        question:
+            "",
+        choice1: "f",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: 4,
+    },
+    {
+        question:
+            "",
+        choice1: "f",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: 4,
+    },
+    {
+        question:
+            "",
+        choice1: "f",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: 4,
+    }
+],
